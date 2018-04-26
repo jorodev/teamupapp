1b8b9e840e5cc82ddc069350405ae5a42c83d404
Georgi Georgiev <joro.dev@gmail.com>
2018-04-26 14:00:39 +0300

    Added form to dashboard's edit profile section

--- resources/views/dashboard/editprofile.blade.php
+++ resources/views/dashboard/editprofile.blade.php
@@ -12,16 +12,81 @@
             <div class="edit-profile">
                 <nav>
                     <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                        <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Profile information</a>
-                        <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Skills</a>
-                        <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Profile image</a>
-                        <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Socials</a>
+                        <a class="nav-item nav-link active" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="true">Profile information</a>
+                        <a class="nav-item nav-link" id="nav-skills-tab" data-toggle="tab" href="#nav-skills" role="tab" aria-controls="nav-skills" aria-selected="false">Skills</a>
+                        <a class="nav-item nav-link" id="nav-profileimg-tab" data-toggle="tab" href="#nav-profileimg" role="tab" aria-controls="nav-profileimg" aria-selected="false">Profile image</a>
+                        <a class="nav-item nav-link" id="nav-socials-tab" data-toggle="tab" href="#nav-socials" role="tab" aria-controls="nav-socials" aria-selected="false">Socials</a>
                     </div>
                 </nav>
                 <div class="tab-content" id="nav-tabContent">
-                    <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">a</div>
-                    <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">b</div>
-                    <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">c</div>
+                    <div class="tab-pane fade show active" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                      <form> 
+                          <div class="form-row">
+                            <div class="form-group col-md-6">
+                              <label for="inputFirstName">First Name</label>
+                              <input type="text" class="form-control" id="inputFirstName" placeholder="First Name">
+                            </div>
+                            <div class="form-group col-md-6">
+                              <label for="inputFirstName">Last Name</label>
+                              <input type="text" class="form-control" id="inputLastName" placeholder="Last Name">
+                            </div>
+                          </div>
+                          <div class="form-row">
+                            <div class="form-group col-md-6">
+                              <label for="inputCountry">Country</label>
+                              <select id="inputCountry" class="form-control">
+                                <option selected>Choose...</option>
+                                <option>Bulgaria</option>
+                              </select>
+                            </div>
+                            <div class="form-group col-md-6">
+                              <label for="inputCity">City</label>
+                              <input type="text" class="form-control" id="inputCity" placeholder="City">
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label for="inputOccupation">Occupation</label>
+                            <input type="text" class="form-control" id="inputOccupation">
+                          </div>
+                          <div class="form-group">
+                            <label for="textArea">Description</label>
+                            <textarea class="form-control" id="textArea" rows="10"></textarea>
+                          </div>
+                          <div class="form-group">
+                            <div class="form-check gender-check">
+                              <div class="row">
+                                <label for="gender">Gender</label>
+                              </div>
+                              <div class="form-group">
+                                <input class="form-check-input" type="checkbox" id="gridCheck">
+                                <label class="form-check-label" for="gridCheck">
+                                  Male
+                                </label>
+                              </div>
+                              <div class="form-group">
+                                <input class="form-check-input" type="checkbox" id="gridCheck">
+                                <label class="form-check-label" for="gridCheck">
+                                  Female
+                                </label>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label for="inputDateOfBirth">Date of Brth</label>
+                            <input type="text" class="form-control" id="inputDateOfBirth" placeholder="DD/MM/YYYY">
+                          </div>
+                          <button type="submit" class="btn btn-primary btn-profile mt-4">Save profile information</button>
+                        </form>
+                    </div>
+                    <div class="tab-pane fade" id="nav-skills" role="tabpanel" aria-labelledby="nav-skills-tab">
+                        b
+                    </div>
+                    <div class="tab-pane fade" id="nav-profileimg" role="tabpanel" aria-labelledby="nav-profileimg-tab">
+                        c
+                    </div>
+                    <div class="tab-pane fade" id="nav-socials" role="tabpanel" aria-labelledby="nav-socials-tab">
+                        d
+                    </div>
                 </div>
             </div>
         </div>
